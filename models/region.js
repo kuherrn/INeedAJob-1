5d66b1815cb400b208afbc12421b68e6a7d8ed1c
ifotn <rich@infrontofthenet.com>
2022-10-07 15:50:37 -0400

    l5/2 - region model

---
+++ models/region.js
@@ -0,0 +1,13 @@
+// import mongoose
+const mongoose = require('mongoose')
+
+// define schema for a Region
+var regionSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: 'Region name is required'
+    }
+})
+
+// make public 
+module.exports = mongoose.Model('Region', regionSchema)
